060a702cf1e39755e0e06e378cba9911f89824f2
Michael Platson <michael.p@osgbilling.com>
2013-07-17 12:19:47 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaGrid::Application.config.secret_token = '77289ebb3e86b81314ae915452614254f1c73e05f09fce2c797aa66ec50d00f158b806e2f808dc4f1708fec560ac45d6c9d2f5760b8eba965a7e56f50f43aed9'
